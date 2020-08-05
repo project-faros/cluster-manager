8368107ae4d762cd86058c57d0769f9bf499a0ff
Ryan Kraus <rmkraus@gmail.com>
2020-08-04 21:51:29 -0400

    Added install-plan cluster command.

---
+++ app/playbooks/install-plan.d/cluster/main.sh
@@ -0,0 +1,24 @@
+PREFIX="/app/cli/"
+STEPS="apply router
+create machines
+apply host-records
+wait-for management-interfaces
+create load-balancer
+create install-repos
+create cluster"
+IFS=$'\n'
+
+for step in $STEPS; do
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    echo '|'
+    echo "|-- ${step}"
+    echo '|'
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
+    /bin/bash -c "${PREFIX}${step} $@"
+    RETCODE=$?
+
+    if [ $RETCODE -ne 0 ]; then
+        exit $RETCODE
+    fi
+done
