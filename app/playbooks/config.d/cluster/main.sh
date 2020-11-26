b803eb40494fc9f8bf7d6d189dd20acdd99c920e
Ryan Kraus <rmkraus@gmail.com>
2020-11-25 19:48:53 -0500

    Hide more errors when sourcing config.

--- app/playbooks/config.d/cluster/main.sh
+++ app/playbooks/config.d/cluster/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /app
-source /data/config.sh
+source /data/config.sh 2> /dev/null
 python3 /app/playbooks/config.d/cluster/config.py
-source /data/config.sh
+source /data/config.sh 2> /dev/null
 python3 /app/inventory.py --verify > /dev/null
