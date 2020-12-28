75ebb94da2999b8bf5d188c1285cfa41f180ab9d
James Harmison <jharmison@redhat.com>
2020-12-28 16:01:04 -0500

    Removed old configuration mechanism
    - Converted various shell calls to proxy only
    - Removed all TUI library elements
    - Removed references to config.sh in playbook scripts
    - Removed config playbook directory

--- app/playbooks/config.d/proxy/main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /app
-source /data/config.sh 2> /dev/null
-source /data/proxy.sh 2> /dev/null
-python3 /app/playbooks/config.d/proxy/config.py
-source /data/config.sh 2> /dev/null
-source /data/proxy.sh 2> /dev/null
-python3 /app/inventory.py --verify > /dev/null
