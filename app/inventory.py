136073b2329eb9b0fd653941aabf811329c77435
James Harmison <jharmison@redhat.com>
2020-12-29 11:31:25 -0500

    Implement new config library in inventory

--- app/inventory.py
+++ app/inventory.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from collections import defaultdict
+from faros_config import FarosConfig
 import ipaddress
 import json
 import os
@@ -9,6 +10,17 @@ import pickle
 
 SSH_PRIVATE_KEY = '/data/id_rsa'
 IP_RESERVATIONS = '/data/ip_addresses'
+
+
+class PydanticEncoder(json.JSONEncoder):
+    def default(self, obj):
+        obj_has_dict = getattr(obj, "dict", False)
+        if obj_has_dict and callable(obj_has_dict):
+            return obj.dict(exclude_none=True)
+        elif isinstance(obj, ipaddress._IPAddressBase):
+            return str(obj)
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 
 class InventoryGroup(object):
@@ -33,7 +45,7 @@ class Inventory(object):
     _data = {"_meta": {"hostvars": defaultdict(dict)}}
 
     def __init__(self, mode=0, host=None):
-        if mode==1:
+        if mode == 1:
             # host info requested
             # current, only list and none are implimented
             raise NotImplementedError()
@@ -41,15 +53,11 @@ class Inventory(object):
         self._mode = mode
         self._host = host
 
-    def __del__(self):
-        if self._mode == 0:
-            print(self.to_json())
-
     def host(self, name):
         return self._data['_meta']['hostvars'].get(name)
 
     def group(self, name):
-        if name in self_data:
+        if name in self._data:
             return InventoryGroup(self, name)
         else:
             return None
@@ -77,18 +85,17 @@ class Inventory(object):
         self._data['_meta']['hostvars'][name].update(hostvars)
 
     def to_json(self):
-        return json.dumps(self._data, sort_keys=True,
-            indent=4, separators=(',', ': '))
+        return json.dumps(self._data, sort_keys=True, indent=4,
+                          separators=(',', ': '), cls=PydanticEncoder)
 
 
 class IPAddressManager(dict):
 
-    def __init__(self, save_file, subnet, subnet_mask):
+    def __init__(self, save_file, subnet):
         super().__init__()
         self._save_file = save_file
 
         # parse the subnet definition into a static and dynamic pool
-        subnet = ipaddress.ip_network(f'{subnet}/{subnet_mask}', strict=False)
         divided = subnet.subnets()
         self._static_pool = next(divided)
         self._dynamic_pool = next(divided)
@@ -106,7 +113,7 @@ class IPAddressManager(dict):
         # load the last saved state
         try:
             restore = pickle.load(open(save_file, 'rb'))
-        except:
+        except:  # noqa: E722
             restore = {}
         self.update(restore)
 
@@ -157,139 +164,167 @@ class IPAddressManager(dict):
 
 
 class Config(object):
-    _last_key = None
+    shim_var_keys = [
+        'WAN_INT',
+        'BASTION_IP_ADDR',
+        'BASTION_INTERFACES',
+        'BASTION_HOST_NAME',
+        'BASTION_SSH_USER',
+        'CLUSTER_DOMAIN',
+        'CLUSTER_NAME',
+        'BOOT_DRIVE',
+    ]
 
-    def __getitem__(self, key):
-        return self.get(key)
-
-    def get(self, key, default=None):
-        self._last_key = key
-        val = os.environ.get(key, default)
-        try:
-            return val.replace('\\n', '\n')
-        except AttributeError:
-            return val
-
-    @property
-    def error(self):
-        return f'\n\033[31mThere was an error parsing the configuration\nPlease check the value for {self._last_key}.\033[0m\n\n'
+    def __init__(self, yaml_file):
+        self.shim_vars = {}
+        for var in self.shim_var_keys:
+            self.shim_vars[var] = os.getenv(var)
+        config = FarosConfig.from_yaml(yaml_file)
+        self.network = config.network
+        self.bastion = config.bastion
+        self.cluster = config.cluster
+        self.proxy = config.proxy
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--list', action = 'store_true')
-    parser.add_argument('--verify', action = 'store_true')
-    parser.add_argument('--host', action = 'store')
+    parser.add_argument('--list', action='store_true')
+    parser.add_argument('--verify', action='store_true')
+    parser.add_argument('--host', action='store')
     args = parser.parse_args()
     return args
 
+
 def main(config, ipam, inv):
     # GATHER INFORMATION FOR EXTRA NODES
-    extra_nodes = json.loads(config.get('EXTRA_NODES', '[]'))
-    for idx, item in enumerate(extra_nodes):
-        addr = ipam.get(item['mac'], item.get('ip'))
-        extra_nodes[idx].update({'ip': addr})
+    for node in config.network.lan.dhcp.extra_reservations:
+        addr = ipam.get(node.mac, str(node.ip))
+        node.ip = ipaddress.IPv4Address(addr)
 
     # CREATE INVENTORY
-    inv.add_group('all', None,
+    inv.add_group(
+        'all', None,
         ansible_ssh_private_key_file=SSH_PRIVATE_KEY,
-        cluster_name=config['CLUSTER_NAME'],
-        cluster_domain=config['CLUSTER_DOMAIN'],
-        admin_password=config['ADMIN_PASSWORD'],
-        pull_secret=json.loads(config['PULL_SECRET']),
-        mgmt_provider=config['MGMT_PROVIDER'],
-        mgmt_user=config['MGMT_USER'],
-        mgmt_password=config['MGMT_PASSWORD'],
-        install_disk=config['BOOT_DRIVE'],
+        cluster_name=config.shim_vars['CLUSTER_NAME'],
+        cluster_domain=config.shim_vars['CLUSTER_DOMAIN'],
+        admin_password=config.bastion.become_pass,
+        pull_secret=json.loads(config.cluster.pull_secret),
+        mgmt_provider=config.cluster.management.provider,
+        mgmt_user=config.cluster.management.user,
+        mgmt_password=config.cluster.management.password,
+        install_disk=config.shim_vars['BOOT_DRIVE'],
         loadbalancer_vip=ipam['loadbalancer'],
         dynamic_ip_range=ipam.dynamic_pool,
         reverse_ptr_zone=ipam.reverse_ptr_zone,
-        subnet=config['SUBNET'],
-        subnet_mask=config['SUBNET_MASK'],
-        wan_ip=config['BASTION_IP_ADDR'],
-        extra_nodes=extra_nodes,
-        ignored_macs=config['IGNORE_MACS'],
-        dns_forwarders=[item['server'] for item in json.loads(config.get('DNS_FORWARDERS', '[]'))],
-        proxy=config['PROXY']=="True",
-        proxy_http=config.get('PROXY_HTTP', ''),
-        proxy_https=config.get('PROXY_HTTPS', ''),
-        proxy_noproxy=[item['dest'] for item in json.loads(config.get('PROXY_NOPROXY', '[]'))],
-        proxy_ca=config.get('PROXY_CA', ''))
+        subnet=str(config.network.lan.subnet.network_address),
+        subnet_mask=config.network.lan.subnet.prefixlen,
+        wan_ip=config.shim_vars['BASTION_IP_ADDR'],
+        extra_nodes=config.network.lan.dhcp.extra_reservations,
+        ignored_macs=config.network.lan.dhcp.ignore_macs,
+        dns_forwarders=config.network.lan.dns_forward_resolvers,
+        proxy=config.proxy is not None,
+        proxy_http=config.proxy.http if config.proxy is not None else '',
+        proxy_https=config.proxy.https if config.proxy is not None else '',
+        proxy_noproxy=config.proxy.noproxy if config.proxy is not None else [],
+        proxy_ca=config.proxy.ca if config.proxy is not None else ''
+    )
 
     infra = inv.add_group('infra')
-    router = infra.add_group('router',
-        wan_interface=config['WAN_INT'],
-        lan_interfaces=json.loads(config['ROUTER_LAN_INT']),
-        all_interfaces=config['BASTION_INTERFACES'].split(),
-        allowed_services=json.loads(config['ALLOWED_SERVICES']))
+    router = infra.add_group(
+        'router',
+        wan_interface=config.shim_vars['WAN_INT'],
+        lan_interfaces=config.network.lan.interfaces,
+        all_interfaces=config.shim_vars['BASTION_INTERFACES'].split(),
+        allowed_services=config.network.port_forward
+    )
     # ROUTER INTERFACES
-    router.add_host('wan',
-        config['BASTION_IP_ADDR'],
-        ansible_become_pass=config['ADMIN_PASSWORD'],
-        ansible_ssh_user=config['BASTION_SSH_USER'])
-    router.add_host('lan',
+    router.add_host(
+        'wan', config.shim_vars['BASTION_IP_ADDR'],
+        ansible_become_pass=config.bastion.become_pass,
+        ansible_ssh_user=config.shim_vars['BASTION_SSH_USER']
+    )
+    router.add_host(
+        'lan',
         ipam['bastion'],
-        ansible_become_pass=config['ADMIN_PASSWORD'],
-        ansible_ssh_user=config['BASTION_SSH_USER'])
+        ansible_become_pass=config.bastion.become_pass,
+        ansible_ssh_user=config.shim_vars['BASTION_SSH_USER']
+    )
     # DNS NODE
-    router.add_host('dns',
+    router.add_host(
+        'dns',
         ipam['bastion'],
-        ansible_become_pass=config['ADMIN_PASSWORD'],
-        ansible_ssh_user=config['BASTION_SSH_USER'])
+        ansible_become_pass=config.bastion.become_pass,
+        ansible_ssh_user=config.shim_vars['BASTION_SSH_USER']
+    )
     # DHCP NODE
-    router.add_host('dhcp',
+    router.add_host(
+        'dhcp',
         ipam['bastion'],
-        ansible_become_pass=config['ADMIN_PASSWORD'],
-        ansible_ssh_user=config['BASTION_SSH_USER'])
+        ansible_become_pass=config.bastion.become_pass,
+        ansible_ssh_user=config.shim_vars['BASTION_SSH_USER']
+    )
     # LOAD BALANCER NODE
-    router.add_host('loadbalancer',
+    router.add_host(
+        'loadbalancer',
         ipam['loadbalancer'],
-        ansible_become_pass=config['ADMIN_PASSWORD'],
-        ansible_ssh_user=config['BASTION_SSH_USER'])
+        ansible_become_pass=config.bastion.become_pass,
+        ansible_ssh_user=config.shim_vars['BASTION_SSH_USER']
+    )
 
     # BASTION NODE
     bastion = infra.add_group('bastion_hosts')
-    bastion.add_host(config['BASTION_HOST_NAME'],
-            ipam['bastion'],
-            ansible_become_pass=config['ADMIN_PASSWORD'],
-            ansible_ssh_user=config['BASTION_SSH_USER'])
+    bastion.add_host(
+        config.shim_vars['BASTION_HOST_NAME'],
+        ipam['bastion'],
+        ansible_become_pass=config.bastion.become_pass,
+        ansible_ssh_user=config.shim_vars['BASTION_SSH_USER']
+    )
 
     # CLUSTER NODES
     cluster = inv.add_group('cluster')
     # BOOTSTRAP NODE
     ip = ipam['bootstrap']
-    cluster.add_host('bootstrap', ip,
-            ansible_ssh_user='core',
-            node_role='bootstrap')
+    cluster.add_host(
+        'bootstrap', ip,
+        ansible_ssh_user='core',
+        node_role='bootstrap'
+    )
     # CLUSTER CONTROL PLANE NODES
     cp = cluster.add_group('control_plane', node_role='master')
-    node_defs = json.loads(config['CP_NODES'])
-    for count, node in enumerate(node_defs):
-        ip = ipam[node['mac']]
-        mgmt_ip = ipam[node['mgmt_mac']]
-        cp.add_host(node['name'], ip,
-           mac_address=node['mac'],
-           mgmt_mac_address=node['mgmt_mac'],
-           mgmt_hostname=mgmt_ip,
-           ansible_ssh_user='core',
-           cp_node_id=count)
-        if node.get('install_drive'):
-            cp.host(node['name'])['install_disk'] = node['install_drive']
+    for count, node in enumerate(config.cluster.nodes):
+        ip = ipam[node.mac]
+        mgmt_ip = ipam[node.mgmt_mac]
+        cp.add_host(
+            node.name, ip,
+            mac_address=node.mac,
+            mgmt_mac_address=node.mgmt_mac,
+            mgmt_hostname=mgmt_ip,
+            ansible_ssh_user='core',
+            cp_node_id=count
+        )
+        if node.install_drive is not None:
+            cp.host(node['name'])['install_disk'] = node.install_drive
 
     # VIRTUAL NODES
-    virt = inv.add_group('virtual',
-            mgmt_provider='kvm',
-            mgmt_hostname='bastion',
-            install_disk='vda')
+    virt = inv.add_group(
+        'virtual',
+        mgmt_provider='kvm',
+        mgmt_hostname='bastion',
+        install_disk='vda'
+    )
     virt.add_host('bootstrap')
 
     # MGMT INTERFACES
-    mgmt = inv.add_group('management',
-        ansible_ssh_user=config['MGMT_USER'],
-        ansible_ssh_pass=config['MGMT_PASSWORD'])
-    for count, node in enumerate(node_defs):
-        mgmt.add_host(node['name'] + '-mgmt', ipam[node['mgmt_mac']],
-                mac_address=node['mgmt_mac'])
+    mgmt = inv.add_group(
+        'management',
+        ansible_ssh_user=config.cluster.management.user,
+        ansible_ssh_pass=config.cluster.management.password
+    )
+    for node in config.cluster.nodes:
+        mgmt.add_host(
+            node.name + '-mgmt', ipam[node.mgmt_mac],
+            mac_address=node.mgmt_mac
+        )
 
 
 if __name__ == "__main__":
@@ -302,13 +337,14 @@ if __name__ == "__main__":
     else:
         mode = 1
 
-    # INITIALIZE CONFIG HANDLER
-    config = Config()
+    # INTIALIZE CONFIG
+    config = Config('/data/config.yml')
 
     # INTIALIZE IPAM
     ipam = IPAddressManager(
         IP_RESERVATIONS,
-        config['SUBNET'], config['SUBNET_MASK'])
+        config.network.lan.subnet
+    )
 
     # INITIALIZE INVENTORY
     inv = Inventory(mode, args.host)
@@ -316,6 +352,9 @@ if __name__ == "__main__":
     # CREATE INVENTORY
     try:
         main(config, ipam, inv)
+        if mode == 0:
+            print(inv.to_json())
+
     except Exception as e:
         if mode == 2:
             sys.stderr.write(config.error)
