5148b4af297c91195465889adcb6cad33686b6fa
Ryan Kraus <rmkraus@gmail.com>
2021-06-30 02:00:48 -0400

    Increase timeout for checking cluster version.

--- app/playbooks/get.d/cluster-upgrade-status/main.sh
+++ app/playbooks/get.d/cluster-upgrade-status/main.sh
@@ -2,6 +2,6 @@
 
 cat /data/upgrade-status 2> /dev/null
 echo; echo;
-timeout 1 oc get clusterversion version --no-headers=true | tr -s ' ' | cut -d ' ' -f5-;
+timeout 3 oc get clusterversion version --no-headers=true | tr -s ' ' | cut -d ' ' -f5-;
 echo; timeout 1 oc get nodes;
 echo; timeout 1 oc get co;
